66d89df75acab8c3c4e0ed0f6fa0dc09d72596cc
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-06-30 17:54:03 +0700

    Melengkapi kategori BMI kelebihan dan obesitas

--- conditionalstatment.cpp
+++ conditionalstatment.cpp
@@ -23,6 +23,17 @@ string cekKondisi(float bmi){
     return "";
 }
 
+string cekKondisi(float bmi){
+    if(bmi < 18.5)
+        return "Berat Badan Kurang";
+    else if(bmi < 25)
+        return "Berat Badan Normal";
+    else if(bmi < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
+
 int main() {
     float berat, tinggi;
 
