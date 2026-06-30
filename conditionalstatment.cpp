bac3103d1ea0fc1dcb367cf5a021a8c7ba1ee2d9
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-06-30 17:33:14 +0700

    Membuat prosedur input data BMI

--- conditionalstatment.cpp
+++ conditionalstatment.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+void inputData(float &berat, float &tinggi){
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
 int main() {
     float berat, tinggi;
+
+    inputData(berat, tinggi);
 
     return 0;
 }
